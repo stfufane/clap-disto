8dac027e5f561b9e21433bbe1627ce1fc100e198
Stéphane Albanese <albanese.stephane@gmail.com>
2025-08-28 00:37:39 +0200

    Add an indicator dot on the knob

--- src/gui/RotaryKnob.cpp
+++ src/gui/RotaryKnob.cpp
@@ -16,9 +16,12 @@ RotaryKnob::RotaryKnob(Disstortion& disstortion, clap_id param_id) : IParamContr
 void RotaryKnob::draw(visage::Canvas& canvas) {
     const auto smaller_size = std::min(width(), height());
     const auto w = smaller_size * 0.8f;
+
     // Center the knob
     const auto margin_x = (width() - w) * .5f;
     const auto margin_y = (height() - w) * .5f;
+    const auto center_x = margin_x + w * 0.5f;
+    const auto center_y = margin_y + w * 0.5f;
 
     // Draw the knob circle
     canvas.setColor(0xffaaff88);
@@ -31,9 +34,15 @@ void RotaryKnob::draw(visage::Canvas& canvas) {
     const auto center_radians = angle + angle_start;
 
     canvas.setColor(0xaa215529);
-    canvas.roundedArc(margin_x, margin_y, w, 3.f, center_radians, angle);
-    canvas.setColor(0xff222222);
-    canvas.flatArc(margin_x, margin_y, w, 3.f, center_radians + angle, .05f);
+
+    // Draw the indicator dot
+    const auto radius = w * 0.5f;
+    const auto dot_location = radius * 0.9f;   // Place the dot at 80% from center
+    const auto dot_x = center_x + std::cos(center_radians + angle) * dot_location;
+    const auto dot_y = center_y + std::sin(center_radians + angle) * dot_location;
+
+    constexpr auto dot_radius = 5.f;
+    canvas.circle(dot_x - dot_radius, dot_y - dot_radius, dot_radius * 2);
 }
 
 void RotaryKnob::mouseDown(const visage::MouseEvent& e) {
@@ -58,7 +67,7 @@ void RotaryKnob::mouseDrag(const visage::MouseEvent& e) {
         return;
     }
 
-        const float dy = mDragStartY - e.position.y;
+    const float dy = mDragStartY - e.position.y;
     mAccumulatedDrag += dy * mRange / mSensitivity;
 
     auto target_value = mDragStartValue + mAccumulatedDrag;
@@ -78,13 +87,13 @@ void RotaryKnob::mouseDrag(const visage::MouseEvent& e) {
         new_value = getMinValue() + target_step * step_size;
     }
 
-        if (new_value != mCurrentValue) {
-            performChange(new_value);
-            mCurrentValue = new_value;
+    if (new_value != mCurrentValue) {
+        performChange(new_value);
+        mCurrentValue = new_value;
         redraw();
-        }
+    }
 
-        mDragStartY = e.position.y;
+    mDragStartY = e.position.y;
 }
 
 } // namespace stfefane::gui
